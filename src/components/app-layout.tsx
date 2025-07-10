c9dde4ef4849b5c470bbf6d8af2816e2b2fbf945
hwimu <choehwimu@gmail.com>
2025-07-10 02:08:46 +0000

    헤더를 상단으로 옮겨줘

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -7,19 +7,21 @@ import { Header } from "./header"
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
      <SidebarProvider>
-      <Sidebar>
-        <SidebarContent>
-          <SidebarMenu />
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset>
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <Header />
-          <main className="flex flex-1 flex-col gap-4 bg-background p-4 sm:px-6 sm:py-0 md:gap-8">
-            {children}
-          </main>
+          <div className="flex">
+            <Sidebar>
+              <SidebarContent>
+                <SidebarMenu />
+              </SidebarContent>
+            </Sidebar>
+            <SidebarInset>
+                <main className="flex flex-1 flex-col gap-4 bg-background p-4 sm:px-6 sm:py-0 md:gap-8">
+                  {children}
+                </main>
+            </SidebarInset>
+          </div>
         </div>
-      </SidebarInset>
     </SidebarProvider>
   )
 }
